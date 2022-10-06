a5338968e02f0a1d46c2eb47dbe3edf1328d7381
woror <dsych44@gmail.com>
2022-10-06 10:18:37 +0200

    Added final policy visualization

--- Utils/produce_output.py
+++ Utils/produce_output.py
@@ -1,17 +1,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle, os
+from Simulations.Utils.misc import my_chdir
 
 
-def draw_all(algorithm, folder):
+def prepare_folders(algorithm, folder):
 	root = os.getcwd()
 	os.chdir(f'../Results/{algorithm}/{folder}')
-	try:
-		os.mkdir('figures')
-	except:
-		pass
+	my_chdir('figures')
+	my_chdir('counts')
+	os.chdir(root)
 
-	os.chdir('queue_data')
+
+def draw_final_policy(algorithm, folder):
+	root = os.getcwd()
+	os.chdir(f'../Results/{algorithm}/{folder}')
+	args = pickle.load(open('args.pickle', 'rb'))
+	os.chdir('policies')
+	final_policy = np.load(f'policy_{len(os.listdir(os.getcwd())) - 1}.npy')
+	os.chdir('../figures')
+	for i, feature in enumerate(['Payment', 'Time', 'Position']):
+		avr_axes = [0, 1, 2]
+		avr_axes.pop(i)
+		avr_policy = np.mean(final_policy, axis=tuple(avr_axes))
+		avr_percentage = np.round(100 * avr_policy, decimals=1)
+		x = np.arange(avr_policy.shape[0])
+		plt.grid(0.25)
+		for action in range(1, args.F + 1):
+			plt.plot(x, avr_percentage[:, action], label=f'Pay {action}')
+
+		plt.xlabel(feature)
+		plt.ylabel('Probability [%]')
+		plt.title(f'Policy as a function of {feature}')
+		plt.ylim(ymin=0)
+		plt.legend()
+		plt.savefig(f'{feature.lower()}_averaged.pdf')
+		plt.clf()
+
+	os.chdir(root)
+
+
+def draw_leaving_counts(algorithm, folder):
+	root = os.getcwd()
+	os.chdir(f'../Results/{algorithm}/{folder}/queue_data')
 	args = pickle.load(open('args.pickle', 'rb'))
 	for num in range(args.train_sims):
 		leaving_time = np.load(f'leaving_time_{num}.npy')
@@ -25,8 +56,7 @@ def draw_all(algorithm, folder):
 		axs[1].bar(np.arange(args.Q + 1), leaving_payment / np.sum(leaving_payment), align='center')
 		axs[1].set_xlabel('Payment')
 		axs[1].set_ylabel('Percentage')
-		plt.savefig(os.pardir + f'/figures/counts_{num}.png')
-		plt.savefig(os.pardir + f'/figures/counts_{num}.pdf')
+		plt.savefig(os.pardir + f'/figures/counts/counts_{num}.pdf')
 		plt.clf()
 
 		average_payment = np.sum(np.arange(args.Q + 1) * leaving_payment / np.sum(leaving_payment))
@@ -36,8 +66,10 @@ def draw_all(algorithm, folder):
 
 
 algorithm = 'PPO'
-folder = '2022-10-05_10-41'
-draw_all(algorithm, folder)
+folder = '2022-10-05_21-00'
+prepare_folders(algorithm, folder)
+#draw_leaving_counts(algorithm, folder)
+draw_final_policy(algorithm, folder)
 
 
 
